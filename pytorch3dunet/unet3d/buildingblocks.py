9046cd560565ff730e9756bdd16695cee556b0c8
jolee <leejung209@gmail.com>
2022-08-15 22:58:05 +0900

    2020-08-15 update

--- pytorch3dunet/unet3d/buildingblocks.py
+++ pytorch3dunet/unet3d/buildingblocks.py
@@ -190,6 +190,54 @@ class ExtResNetBlock(nn.Module):
         return out
 
 
+class DropResNetBlock(nn.Module):
+    """
+    Basic UNet block consisting of a SingleConv followed by the residual block and Dropout
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size=3, order='cge', num_groups=8, **kwargs):
+        super(DropResNetBlock, self).__init__()
+
+        # first convolution
+        self.conv1 = SingleConv(in_channels, out_channels, kernel_size=kernel_size, order=order, num_groups=num_groups)
+        # residual block
+        self.conv2 = SingleConv(out_channels, out_channels, kernel_size=kernel_size, order=order, num_groups=num_groups)
+        # remove non-linearity from the 3rd convolution since it's going to be applied after adding the residual
+        n_order = order
+        for c in 'rel':
+            n_order = n_order.replace(c, '')
+        self.conv3 = SingleConv(out_channels, out_channels, kernel_size=kernel_size, order=n_order,
+                                num_groups=num_groups)
+        
+        # dropout
+        self.dropout = nn.Dropout()
+        
+        # create non-linearity separately
+        if 'l' in order:
+            self.non_linearity = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+        elif 'e' in order:
+            self.non_linearity = nn.ELU(inplace=True)
+        else:
+            self.non_linearity = nn.ReLU(inplace=True)
+
+
+    def forward(self, x):
+        # apply first convolution and save the output as a residual
+        out = self.conv1(x)
+        residual = out
+
+        # residual block
+        out = self.conv2(out)
+        out = self.conv3(out)
+
+        out += residual
+        out = self.dropout(out)
+        out = self.non_linearity(out)
+
+        return out
+    
+    
+
 class Encoder(nn.Module):
     """
     A single module from the encoder path consisting of the optional max
